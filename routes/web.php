fd0c3988d0a34f9dc7537dfcee789546da403ff2
Raul Tenorio <raul.tenorio@finantech.com.ec>
2022-07-10 09:31:26 -0500

    Mix Templates Update

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,17 @@ use Inertia\Inertia;
 |
 */
 
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -29,3 +33,21 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/sugerencias', [SugerenciasController::class, 'sendMails'])->name('sugerencias');
+
+Route::post('/sugerencias', [SugerenciasController::class, 'sendMailsProcessing'])->name('sugerencias.form');
+
+Route::get('/mensaje', [MessageController::class,'index'])->name('mensaje')->middleware('auth');
+
+Route::post('/mensaje', [MessageController::class,'store'])->name('mensaje.store')->middleware('auth');
+
+Route::get('/notificaciones', [NotificacionController::class,'index'])->name('notificacion.all')->middleware('auth');
+
+Route::get('/notificaciones/{id}', [NotificacionController::class,'show'])->name('notificacion.detalle')->middleware('auth');
+
+Route::patch('/notificaciones/{id}', [NotificacionController::class,'update'])->name('notificacion.realizada')->middleware('auth');
+
+Route::delete('/notificaciones/{id}', [NotificacionController::class,'destroy'])->name('notificacion.terminada')->middleware('auth');
