5fc55346361382d522013b061234831fbc5eb35e
phob0z <leonel.molina@epn.edu.ec>
2022-07-13 16:17:51 -0500

    Fix css/js

--- resources/views/mensajeEntregado.blade.php
+++ resources/views/mensajeEntregado.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @notifyCss
-     <div class="container mt-5 col-12 col-sm-6">
+    <div class="container mt-5 col-12 col-sm-6">
         <br><br>
         <p class="text text-center h2">Mail entregado satisfactoriamente</p>
         <br>
@@ -10,9 +10,8 @@
     <br>
     <hr class="featurette-divider">
     <br>
-    <div class="image-container" >
-        <img src="https://www.megaidea.net/wp-content/uploads/2018/07/DORAEMON-20.png" width="50%" height="350px"/>
+    <div class="image-container">
+        <img src="https://www.megaidea.net/wp-content/uploads/2018/07/DORAEMON-20.png" width="50%" height="350px" />
     </div>
     @notifyJs
-    @endsection
-
+@endsection
